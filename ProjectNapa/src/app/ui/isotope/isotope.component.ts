a83801234a080a188f39937c31c01a6db68be55c
NPG-Ba <45417724+NPG-Ba@users.noreply.github.com>
2018-12-19 15:06:18 +0700

    isotope -->done
    Còn phần  (resposive)

--- ProjectNapa/src/app/ui/isotope/isotope.component.ts
+++ ProjectNapa/src/app/ui/isotope/isotope.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 declare var Isotope: any;
 
 @Component({
@@ -6,45 +6,50 @@ declare var Isotope: any;
   templateUrl: './isotope.component.html',
   styleUrls: ['./isotope.component.css']
 })
-export class IsotopeComponent implements OnInit {
-
+export class IsotopeComponent implements OnInit, AfterViewInit {
   profile = [
-              {image :'assets/images/demo/web1.jpeg', title:'CRM System',content:'Create a market cap service for Kyber Network coin base on decentralized.',tag:'Blockchain Solutions'},
-              {image :'assets/images/demo/block4.jpg', title:'Trading system',content:'Create a market cap service for Kyber Network coin base on decentralized.',tag:'Web Development'},
-              {image :'assets/images/demo/block3.jpg', title:'Data Entry App',content:'Create a market cap service for Kyber Network coin base on decentralized.',tag:'Backend Applications'},
-              {image :'assets/images/demo/web1.jpeg', title:'TO Corporation',content:'Create a market cap service for Kyber Network coin base on decentralized.',tag:'Apps Development'},
-              {image :'assets/images/demo/MyF.jpeg', title:'FS Project',content:'Create a market cap service for Kyber Network coin base on decentralized.',tag:'BPO Services'},
-              {image :'assets/images/demo/areaparking_app.png', title:'Auto Parking',content:'Create a market cap service for Kyber Network coin base on decentralized.',tag:'Blockchain Solutions'},
-              {image :'assets/images/demo/adas.jpeg', title:'Traveling Company',content:'Create a market cap service for Kyber Network coin base on decentralized.',tag:'Apps Development'},
-              {image :'assets/images/demo/data.jpeg', title:'Create Wallet',content:'Create a market cap service for Kyber Network coin base on decentralized.',tag:'Apps Development'}
-            ];
-  tags =    [
-               {name:'Blockchain Solutions',des:'Blockchain Solutions'},
-               {name:'Web Development',des:'Blockchain Solutions'},
-               {name:'Backend Applications',des:'Backend Applications'},
-               {name:'Apps Development',des:'Apps Development'},
-               {name:'BPO Services',des:'BPO Services'}
+    { clazz: 'web', image: 'assets/images/demo/web1.jpeg', title: 'CRM System', content: 'Create a market cap service for Kyber Network coin base on decentralized.' },
+    { clazz: 'blockchain', image: 'assets/images/demo/block4.jpg', title: 'Trading system', content: 'Create a market cap service for Kyber Network coin base on decentralized.' },
+    { clazz: 'blockchain', image: 'assets/images/demo/block3.jpg', title: 'Data Entry App', content: 'Create a market cap service for Kyber Network coin base on decentralized.'},
+    { clazz: 'backend', image: 'assets/images/demo/web1.jpeg', title: 'TO Corporation', content: 'Create a market cap service for Kyber Network coin base on decentralized.' },
+    { clazz: 'app', image: 'assets/images/demo/MyF.jpeg', title: 'FS Project', content: 'Create a market cap service for Kyber Network coin base on decentralized.'},
+    { clazz: 'web', image: 'assets/images/demo/areaparking_app.png', title: 'Auto Parking', content: 'Create a market cap service for Kyber Network coin base on decentralized.'},
+    { clazz: 'bpo', image: 'assets/images/demo/adas.jpeg', title: 'Traveling Company', content: 'Create a market cap service for Kyber Network coin base on decentralized.' },
+    { clazz: 'app', image: 'assets/images/demo/data.jpeg', title: 'Create Wallet', content: 'Create a market cap service for Kyber Network coin base on decentralized.'}
+  ];
+  tags = [
+    { name: 'Blockchain Solutions', clazz: 'blockchain' },
+    { name: 'Web Development', clazz: 'web' },
+    { name: 'Backend Applications', clazz: 'backend' },
+    { name: 'Apps Development', clazz: 'app' },
+    { name: 'BPO Services', clazz: 'bpo' }
 
-            ];
+  ];
   els: any;
   iso: any;
-  
+
   constructor(private hostElement: ElementRef) {
     this.els = hostElement;
   }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
     this.iso = new Isotope(this.els.nativeElement.querySelector('.grid'),
       {
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows',
         sortBy: 'random'
       }
     );
   }
 
-  filter(field){
-    console.log(field);
+  filter(field) {
+    //console.log(field);
     this.iso.arrange({
       filter: '.' + field
     });
-  } 
+  }
 }
