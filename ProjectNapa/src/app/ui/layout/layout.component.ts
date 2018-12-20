b737cd42996f9428ccff226c36a39c0099c95e1c
NPG-Ba <45417724+NPG-Ba@users.noreply.github.com>
2018-12-20 13:05:37 +0700

    slide
    20/12

--- ProjectNapa/src/app/ui/layout/layout.component.ts
+++ ProjectNapa/src/app/ui/layout/layout.component.ts
@@ -1,21 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class LayoutComponent implements OnInit {
+
+
 
   ngOnInit() {
   }
   closeResult: string;
 
-  constructor(private modalService: NgbModal) {}
+  images =[
+            {src:'assets/images/demo/slider/photo5.jpg',title:'BUILD YOUR WEB AND MOBILE APPS',des:'NAPA Global Helps You Realize Your Vision'},
+            {src:'assets/images/demo/slider/a2.jpg',title:'BUILD YOUR WEB AND MOBILE APPS',des:'NAPA Global Helps You Realize Your Vision'},
+            {src:'assets/images/demo/slider/computer-drink-iphone-586339.jpg',title:'BUILD YOUR WEB AND MOBILE APPS',des:'NAPA Global Helps You Realize Your Vision'}
+          ];
+
+
+  constructor(private modalService: NgbModal,config: NgbCarouselConfig) 
+  {
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+    config.showNavigationIndicators= false;
+    config.showNavigationArrows= false;
+  }
 
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-center-title'}).result.then((result) => {
+    this.modalService.open(content, { centered: true }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
